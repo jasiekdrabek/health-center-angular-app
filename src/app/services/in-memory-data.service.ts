c57311c18461dafa055bffb6c9fdb1070d8e9b1a
jasiekdrabek <33521261+jasiekdrabek@users.noreply.github.com>
2023-01-21 14:28:42 +0100

    .

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { MedicalReferral } from '../interfaces/medicalReferral';
 import { MedicalVisit } from '../interfaces/medicalVisit';
 import { Medicine } from '../interfaces/medicine';
